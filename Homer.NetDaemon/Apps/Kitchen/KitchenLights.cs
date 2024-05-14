d7a13f32471695d84312a98c7bb6a43557d4b98a
Qin Guan <qinguan20040914@gmail.com>
2024-05-14 21:46:37 +0800

    feat: kitchen lights

---
+++ Homer.NetDaemon/Apps/Kitchen/KitchenLights.cs
@@ -0,0 +1,48 @@
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using Homer.NetDaemon.Apps.LivingRoom;
+using Homer.NetDaemon.Entities;
+using NetDaemon.AppModel;
+using NetDaemon.HassModel;
+using NetDaemon.HassModel.Entities;
+
+namespace Homer.NetDaemon.Apps.Kitchen;
+
+[NetDaemonApp]
+[Focus]
+public class KitchenLights
+{
+    public KitchenLights(ILogger<KitchenLights> logger, IScheduler scheduler, BinarySensorEntities bse,
+        SwitchEntities se, SensorEntities sensors)
+    {
+        bse.KitchenTuyaPresencePresence.StateChanges().Subscribe(Console.WriteLine);
+        bse.KitchenTuyaPresencePresence.StateChanges()
+            .WhenStateIsFor(
+                e => e.IsOff(),
+                TimeSpan.FromMinutes(1),
+                scheduler
+            )
+            .Subscribe(_ =>
+            {
+                se.KitchenLightsLeft.TurnOff();
+                se.KitchenLightsRight.TurnOff();
+            });
+
+        bse.KitchenTuyaPresencePresence.StateChanges()
+            .Where(e => e.New.IsOn())
+            .Subscribe(e =>
+            {
+                if (TimeOnly.FromDateTime(DateTime.Now) > new TimeOnly(22, 0)
+                    && TimeOnly.FromDateTime(DateTime.Now) < new TimeOnly(6, 0)
+                   )
+                    se.KitchenLightsLeft.TurnOn();
+                else
+                {
+                    if (sensors.PresenceSensorFp2B4c4LightSensorLightLevel.State < 60)
+                    {
+                        se.KitchenLightsRight.TurnOn();
+                    }
+                }
+            });
+    }
+}
--- Homer.NetDaemon/Apps/LivingRoom/LivingRoomLights.cs
+++ Homer.NetDaemon/Apps/LivingRoom/LivingRoomLights.cs
@@ -8,7 +8,5 @@ public class LivingRoomLights
 {
     public LivingRoomLights(ILogger<LivingRoomLights> logger, BinarySensorEntities bse)
     {
-        bse.PresenceSensorFp2B4c4PresenceSensor1.StateChanges()
-            .Subscribe(e => logger.LogInformation("motion in living room: {E}", e));
     }
 }
