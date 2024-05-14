6aa7c5963980614ae5924489ee362e8bb84b8769
Qin Guan <qinguan20040914@gmail.com>
2024-05-15 01:34:34 +0800

    feat: kdk

--- Homer.NetDaemon/Apps/LivingRoom/LivingRoomLights.cs
+++ Homer.NetDaemon/Apps/LivingRoom/LivingRoomLights.cs
@@ -1,33 +1,18 @@
-using System.Reactive.Linq;
 using Homer.NetDaemon.Entities;
 using NetDaemon.AppModel;
-using NetDaemon.HassModel.Entities;
 
 namespace Homer.NetDaemon.Apps.LivingRoom;
 
 [NetDaemonApp]
 public class LivingRoomLights
 {
-    public LivingRoomLights(ILogger<LivingRoomLights> logger, BinarySensorEntities binarySensorEntities, RemoteEntities remoteEntities)
+    public LivingRoomLights(
+        ILogger<LivingRoomLights> logger, 
+        InputBooleanEntities inputBooleanEntities,
+        RemoteEntities remoteEntities
+    )
     {
-        List<BinarySensorEntity> fanSensors =
-        [
-            binarySensorEntities.PresenceSensorFp2B4c4PresenceSensor2,
-            binarySensorEntities.PresenceSensorFp2B4c4PresenceSensor3,
-            binarySensorEntities.PresenceSensorFp2B4c4PresenceSensor4,
-            binarySensorEntities.PresenceSensorFp2B4c4PresenceSensor5,
-            binarySensorEntities.PresenceSensorFp2B4c4PresenceSensor6,
-            binarySensorEntities.PresenceSensorFp2B4c4PresenceSensor8,
-        ];
-
-        foreach (var sensor in fanSensors)
-        {
-            sensor.StateChanges()
-                .Where(s => s.New.IsOn())
-                .Subscribe(_ =>
-                {
-                    remoteEntities.LivingRoomRemote.SendCommand("Light Power", "Living Room KDK");
-                });
-        }
+        inputBooleanEntities.LivingRoomFanLights.StateChanges()
+            .Subscribe(_ => { remoteEntities.LivingRoomRemote.SendCommand("Light Power", "Living Room KDK"); });
     }
 }
