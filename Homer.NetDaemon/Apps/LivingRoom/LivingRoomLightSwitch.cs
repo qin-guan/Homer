3c595f5daa486dbf6992c55364310482c4b76a89
Qin Guan <qinguan20040914@gmail.com>
2024-05-16 20:13:47 +0800

    feat: light switch

--- Homer.NetDaemon/Apps/LivingRoom/LivingRoomLightSwitch.cs
+++ Homer.NetDaemon/Apps/LivingRoom/LivingRoomLightSwitch.cs
@@ -8,20 +8,15 @@ namespace Homer.NetDaemon.Apps.LivingRoom;
 [Focus]
 public class LivingRoomLightSwitch
 {
-    public LivingRoomLightSwitch(SensorEntities sensorEntities, SwitchEntities switchEntities)
+    public LivingRoomLightSwitch(SensorEntities sensorEntities, SwitchEntities switchEntities,
+        RemoteEntities remoteEntities)
     {
         sensorEntities.LivingRoomLightsAction.StateChanges()
             .Where(e => e.Entity.State == "single_center")
-            .Subscribe(e =>
-            {
-                switchEntities.LivingRoomLightsCenter.Toggle();
-            });
-        
+            .Subscribe(e => { remoteEntities.LivingRoomRemote.SendCommand("Light Power", "Living Room KDK"); });
+
         sensorEntities.LivingRoomLightsAction.StateChanges()
             .Where(e => e.Entity.State == "double_center")
-            .Subscribe(e =>
-            {
-                switchEntities.DiningTableLights.Toggle();
-            });
+            .Subscribe(e => { switchEntities.DiningTableLights.Toggle(); });
     }
 }
