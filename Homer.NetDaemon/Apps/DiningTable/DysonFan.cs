76698409cca8066bbd4fade976c00d36dde72813
Qin Guan <qinguan20040914@gmail.com>
2024-05-16 11:56:19 +0800

    feat: dining table

---
+++ Homer.NetDaemon/Apps/DiningTable/DysonFan.cs
@@ -0,0 +1,33 @@
+using System.Reactive.Concurrency;
+using Homer.NetDaemon.Entities;
+using NetDaemon.AppModel;
+using NetDaemon.HassModel;
+using NetDaemon.HassModel.Entities;
+
+namespace Homer.NetDaemon.Apps.DiningTable;
+
+[NetDaemonApp]
+public class DysonFan
+{
+    public DysonFan(IScheduler scheduler, IrRemoteLock irRemoteLock, SwitchEntities switchEntities, BinarySensorEntities binarySensorEntities, RemoteEntities remoteEntities)
+    {
+        binarySensorEntities.PresenceSensorFp2B4c4PresenceSensor5.StateChanges()
+            .WhenStateIsFor(e => e.IsOn(), TimeSpan.FromSeconds(15), scheduler)
+            .SubscribeAsync(async e =>
+            {
+                switchEntities.LivingRoomIkeaPlug.TurnOn();
+                await Task.Delay(500);
+                await irRemoteLock.SemaphoreSlim.WaitAsync();
+                remoteEntities.LivingRoomRemote.SendCommand("Power", "Dyson");
+                await Task.Delay(500);
+                irRemoteLock.SemaphoreSlim.Release();
+            });
+        
+        binarySensorEntities.PresenceSensorFp2B4c4PresenceSensor5.StateChanges()
+            .WhenStateIsFor(e => e.IsOff(), TimeSpan.FromMinutes(2), scheduler)
+            .Subscribe(e =>
+            {
+                switchEntities.LivingRoomIkeaPlug.TurnOff();
+            });
+    }
+}
