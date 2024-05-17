2d509a5025afaee69004e1146ebf503934f582b8
Qin Guan <qinguan20040914@gmail.com>
2024-05-17 21:59:26 +0800

    feat: chaos

--- Homer.NetDaemon/Apps/DefaultApp.cs
+++ Homer.NetDaemon/Apps/DefaultApp.cs
@@ -1,12 +1,19 @@
+using Homer.ServiceDefaults.Metrics;
 using NetDaemon.AppModel;
+using NetDaemon.HassModel;
 
 namespace Homer.NetDaemon.Apps;
 
 [NetDaemonApp]
 public class DefaultApp
 {
-    public DefaultApp(ILogger<DefaultApp> logger)
+    public DefaultApp(ILogger<DefaultApp> logger, IHaContext haContext)
     {
+        var eventsProcessedMeter =
+            EntityMetrics.MeterInstance.CreateCounter<int>("homer.netdaemon.homeassistant.events_processed");
+
+        haContext.Events.Subscribe(_ => { eventsProcessedMeter.Add(1); });
+
         logger.LogInformation("Hello, home!");
     }
 }
