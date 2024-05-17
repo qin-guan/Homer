e09b58c87896215bd49f0b17493794711029b505
Qin Guan <qinguan20040914@gmail.com>
2024-05-18 01:36:25 +0800

    fix: no name

--- Homer.NetDaemon/Apps/DefaultApp.cs
+++ Homer.NetDaemon/Apps/DefaultApp.cs
@@ -19,12 +19,7 @@ public class DefaultApp
             {
                 eventsProcessedMeter.Add(1, [
                     new KeyValuePair<string, object?>("entity_id",
-                        e.DataElement.Value.GetProperty("entity_id").GetString()),
-                    new KeyValuePair<string, object?>("friendly_name",
-                        e.DataElement.Value.GetProperty("new_state")
-                            .GetProperty("attributes")
-                            .GetProperty("friendly_name")
-                            .GetString())
+                        e.DataElement.Value.GetProperty("entity_id").GetString())
                 ]);
             }
             else
