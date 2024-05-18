252adf0f2c8390b09c58727ba42704cc5f52ce7a
Qin Guan <qinguan20040914@gmail.com>
2024-05-19 00:23:04 +0800

    chore: jank

--- Homer.NetDaemon/Apps/Bathroom/BathroomPresence.cs
+++ Homer.NetDaemon/Apps/Bathroom/BathroomPresence.cs
@@ -1,67 +1,74 @@
-// using System.Reactive.Linq;
-// using Homer.NetDaemon.Entities;
-// using Homer.NetDaemon.Presence;
-// using NetDaemon.AppModel;
-// using NetDaemon.HassModel.Entities;
-//
-// namespace Homer.NetDaemon.Apps.Bathroom;
-//
-// [NetDaemonApp]
-// public class BathroomPresence
-// {
-//     private WaspState WaspState = WaspState.NoWasp;
-//     private bool Occupied = false;
-//     private DateTime LastMotion;
-//
-//     public BathroomPresence(BinarySensorEntities binarySensorEntities, SwitchEntities switchEntities)
-//     {
-//         var motionSensors = new List<BinarySensorEntity>
-//         {
-//             binarySensorEntities.BathroomSinkMotionOccupancy,
-//             binarySensorEntities.BathroomDoorMotionOccupancy
-//         };
-//
-//         var contactSensors = new List<BinarySensorEntity>
-//         {
-//             binarySensorEntities.BathroomContactSensorContact
-//         };
-//
-//         // Door opened
-//         contactSensors.Select(e => e.StateChanges()).Merge()
-//             .Where(e => e.New.IsOff())
-//             .Subscribe(e => { });
-//
-//         // Motion detected
-//         motionSensors.Select(e => e.StateChanges()).Merge()
-//             .Where(e => e.New.IsOn())
-//             .Subscribe(e =>
-//             {
-//                 LastMotion = DateTime.Now;
-//
-//                 if (Occupied) return;
-//
-//                 Occupied = true;
-//                 switchEntities.BathroomLightsCenter.TurnOn();
-//             });
-//
-//         // Motion cleared
-//         motionSensors.Select(e => e.StateChanges()).Merge()
-//             .Where(e => e.New.IsOff())
-//             .Subscribe(e =>
-//             {
-//                 if (WaspState == WaspState.Wasp)
-//                 {
-//                     WaspState = WaspState.WaspInBox;
-//                 }
-//             });
-//
-//         void Trigger(bool isFromClear)
-//         {
-//             if (Occupied && (((isFromClear) && ((contactSensors.Any(e => e.IsOn())) ||
-//                                                 (contactSensors.All(e => e.IsOff()) &&
-//                                                  LastMotion < contactSensors.Max(e => e.EntityState?.LastChanged) &&
-//                                                  (motionSensors.Max(e => e.EntityState.LastChanged).Value
-//                                                      .Subtract(TimeSpan.FromSeconds(10)))))) || ()))
-//         }
-//     }
-// }
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using Homer.NetDaemon.Entities;
+using Homer.NetDaemon.Presence;
+using NetDaemon.AppModel;
+using NetDaemon.Extensions.Scheduler;
+using NetDaemon.HassModel;
+using NetDaemon.HassModel.Entities;
+
+namespace Homer.NetDaemon.Apps.Bathroom;
+
+[Focus]
+[NetDaemonApp]
+public class BathroomPresence
+{
+    private string CurrentWaspState = Presence.WaspState.STATE_NO_WASP_IN_BOX;
+    private readonly TimeSpan Delay = TimeSpan.FromSeconds(10);
+    private readonly List<BinarySensorEntity> boxSensors;
+    private readonly List<BinarySensorEntity> waspSensors;
+
+    private bool Occupied = false;
+    private DateTime LastMotion;
+
+    public BathroomPresence(INetDaemonScheduler scheduler, BinarySensorEntities binarySensorEntities,
+        SwitchEntities switchEntities)
+    {
+        waspSensors =
+        [
+            binarySensorEntities.BathroomSinkMotionOccupancy,
+            binarySensorEntities.BathroomDoorMotionOccupancy
+        ];
+
+        boxSensors =
+        [
+            binarySensorEntities.BathroomContactSensorContact
+        ];
+
+        boxSensors.Select(e => e.StateChanges()).Merge().Subscribe(e =>
+        {
+            CurrentWaspState = WaspState.STATE_NO_WASP_IN_BOX;
+            scheduler.RunIn(Delay, () => { WaspInABox(e.New.State, getWaspState()); });
+        });
+
+        waspSensors.Select(e => e.StateChanges())
+            .Merge()
+            .Subscribe(e => { WaspInABox(getBoxState(), e.New.State); });
+
+        WaspInABox(getBoxState(), getWaspState());
+    }
+
+    private void WaspInABox(string boxState, string waspState)
+    {
+        if (waspState == WaspState.STATE_WASP)
+        {
+            CurrentWaspState = WaspState.STATE_WASP_IN_BOX;
+            Console.WriteLine("{0}{1}{2}", boxState, waspState, CurrentWaspState);
+        }
+        else if (boxState == WaspState.STATE_BOX_OPEN)
+        {
+            CurrentWaspState = WaspState.STATE_NO_WASP_IN_BOX;
+            Console.WriteLine("{0}{1}{2}", boxState, waspState, CurrentWaspState);
+        }
+    }
+
+    private string getBoxState()
+    {
+        return boxSensors.Any(entity => entity.IsOff()) ? WaspState.STATE_BOX_OPEN : WaspState.STATE_BOX_CLOSED;
+    }
+
+    private string getWaspState()
+    {
+        return waspSensors.Any(entity => entity.IsOn()) ? WaspState.STATE_WASP : WaspState.STATE_NO_WASP;
+    }
+}
