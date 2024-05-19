b566ea6a9f1eefc13ed633d1b59920c61d5e84b1
Qin Guan <qinguan20040914@gmail.com>
2024-05-19 17:08:11 +0800

    feat: bedroom 4

---
+++ Homer.NetDaemon/Apps/Bedroom4/Bedroom4Lights.cs
@@ -0,0 +1,23 @@
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using Homer.NetDaemon.Entities;
+using NetDaemon.AppModel;
+using NetDaemon.HassModel;
+using NetDaemon.HassModel.Entities;
+
+namespace Homer.NetDaemon.Apps.Bedroom4;
+
+[NetDaemonApp]
+public class Bedroom4Lights
+{
+    public Bedroom4Lights(IScheduler scheduler, SensorEntities sensorEntities, SwitchEntities switchEntities)
+    {
+        sensorEntities.ScreekHumanSensor2a06ead0Zone1TargetCounts.StateChanges()
+            .Where(e => e.Entity.State > 0)
+            .Subscribe(_ => { switchEntities.Bedroom4Lights.TurnOn(); });
+
+        sensorEntities.ScreekHumanSensor2a06ead0Zone1TargetCounts.StateChanges()
+            .WhenStateIsFor(e => e?.State == 0, TimeSpan.FromSeconds(15), scheduler)
+            .Subscribe(_ => { switchEntities.Bedroom4Lights.TurnOff(); });
+    }
+}
