f768b2b66bc993c5423619efe846cc6ecaa6a076
Qin Guan <qinguan20040914@gmail.com>
2024-05-21 01:14:33 +0800

    fix: i will fix this

--- Homer.NetDaemon/Apps/Bedroom4/Bedroom4Lights.cs
+++ Homer.NetDaemon/Apps/Bedroom4/Bedroom4Lights.cs
@@ -2,22 +2,25 @@ using System.Reactive.Concurrency;
 using System.Reactive.Linq;
 using Homer.NetDaemon.Entities;
 using NetDaemon.AppModel;
-using NetDaemon.HassModel;
-using NetDaemon.HassModel.Entities;
 
 namespace Homer.NetDaemon.Apps.Bedroom4;
 
 [NetDaemonApp]
 public class Bedroom4Lights
 {
-    public Bedroom4Lights(IScheduler scheduler, SensorEntities sensorEntities, SwitchEntities switchEntities)
+    public Bedroom4Lights(
+        IScheduler scheduler,
+        SensorEntities sensorEntities,
+        SwitchEntities switchEntities,
+        ClimateEntities climateEntities
+    )
     {
         sensorEntities.ScreekHumanSensor2a06ead0Zone1TargetCounts.StateChanges()
             .Where(e => e.Entity.State > 0)
             .Subscribe(_ => { switchEntities.Bedroom4Lights.TurnOn(); });
-
-        sensorEntities.ScreekHumanSensor2a06ead0Zone1TargetCounts.StateChanges()
-            .Where(e => e.Entity.State == 0)
-            .Subscribe(_ => { switchEntities.Bedroom4Lights.TurnOff(); });
+        //
+        // sensorEntities.ScreekHumanSensor2a06ead0Zone1TargetCounts.StateChanges()
+        //     .Where(e => e.Entity.State == 0)
+        //     .Subscribe(_ => { switchEntities.Bedroom4Lights.TurnOff(); });
     }
 }
