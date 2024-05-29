d6bf8763f196e5193a20b975803a4fa5997f3026
Qin Guan <qinguan20040914@gmail.com>
2024-05-29 20:58:21 +0800

    fix: critical logic flaw

--- Homer.NetDaemon/Apps/Bathroom/BathroomPresence.cs
+++ Homer.NetDaemon/Apps/Bathroom/BathroomPresence.cs
@@ -31,7 +31,7 @@ public class BathroomPresence : Occupancy
                     _switchEntities.BathroomLightsCenter
                 ],
                 
-                var value when value >= new TimeOnly(18, 0) && value < new TimeOnly(0, 0) =>
+                var value when value >= new TimeOnly(18, 0) && value < new TimeOnly(23, 59) =>
                 [
                     _switchEntities.BathroomLightsCenter, _switchEntities.BathroomLightsLeft
                 ],
