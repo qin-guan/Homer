94d8879ada77ddab8f8050c349600028b9e17537
Qin Guan <qinguan20040914@gmail.com>
2024-06-10 07:52:45 +0800

    feat: my lights

--- Homer.NetDaemon/Apps/Bedroom4/Bedroom4Lights.cs
+++ Homer.NetDaemon/Apps/Bedroom4/Bedroom4Lights.cs
@@ -3,6 +3,7 @@ using System.Reactive.Linq;
 using Homer.NetDaemon.Apps.Core;
 using Homer.NetDaemon.Entities;
 using NetDaemon.AppModel;
+using NetDaemon.Extensions.Scheduler;
 using NetDaemon.HassModel;
 using NetDaemon.HassModel.Entities;
 
@@ -11,27 +12,69 @@ namespace Homer.NetDaemon.Apps.Bedroom4;
 [NetDaemonApp]
 public class Bedroom4Lights : Occupancy
 {
+    private readonly SensorEntities _sensorEntities;
+    public bool TooBright => _sensorEntities.PresenceSensorFp2B4c4LightSensorLightLevel.State > 30;
+
     public Bedroom4Lights(
         IScheduler scheduler,
+        INetDaemonScheduler netDaemonScheduler,
         InputDatetimeEntities inputDatetimeEntities,
         InputBooleanEntities inputBooleanEntities,
-        BinarySensorEntities contactSensors,
-        BinarySensorEntities motionSensors,
-        SwitchEntities switchEntities
+        BinarySensorEntities binarySensorEntities,
+        SensorEntities sensorEntities,
+        SwitchEntities switchEntities,
+        RemoteEntities remoteEntities,
+        ClimateEntities climateEntities
     ) : base(
         inputDatetimeEntities.Bedroom4LastPresence,
         inputBooleanEntities.Bedroom4Presence,
-        [contactSensors.Bedroom4DoorContact],
-        [motionSensors.ScreekHumanSensor2a06ead0Zone1Presence],
+        [binarySensorEntities.Bedroom4DoorContact],
+        [binarySensorEntities.ScreekHumanSensor2a06ead0Zone1Presence],
         TimeSpan.FromSeconds(5)
     )
     {
-        inputBooleanEntities.Bedroom4Presence.StateChanges()
-            .Where(e => e.Entity.IsOn())
-            .Subscribe(_ => { switchEntities.Bedroom4Lights.TurnOn(); });
+        _sensorEntities = sensorEntities;
 
         inputBooleanEntities.Bedroom4Presence.StateChanges()
-            .WhenStateIsFor(e => e.IsOff(), TimeSpan.FromMinutes(1), scheduler)
-            .Subscribe(_ => { switchEntities.Bedroom4Lights.TurnOff(); });
+            .Where(e => e.Entity.IsOn())
+            .Subscribe(_ =>
+            {
+                if (TooBright)
+                {
+                    remoteEntities.Bedroom4Remote.SendCommand("Light Power", "Bedroom 4 Fanco");
+                }
+
+                remoteEntities.Bedroom4Remote.SendCommand("Fan 1", "Bedroom 4 Fanco");
+            });
+
+        climateEntities.Daikinap97235.StateChanges()
+            .Where(s => s.New.IsOn())
+            .Subscribe(_ =>
+            {
+                if (inputBooleanEntities.Bedroom4Presence.IsOff()) return;
+
+                remoteEntities.Bedroom4Remote.SendCommand("Fan 4", "Bedroom 4 Fanco");
+
+                netDaemonScheduler.RunIn(TimeSpan.FromMinutes(5),
+                    () => { remoteEntities.Bedroom4Remote.SendCommand("Fan 1", "Bedroom 4 Fanco"); });
+            });
+
+        climateEntities.Daikinap97235.StateChanges()
+            .Where(s => s.New.IsOff())
+            .Subscribe(_ =>
+            {
+                if (inputBooleanEntities.Bedroom4Presence.IsOff()) return;
+
+                netDaemonScheduler.RunIn(TimeSpan.FromHours(2),
+                    () => { remoteEntities.Bedroom4Remote.SendCommand("Fan 3", "Bedroom 4 Fanco"); });
+            });
+
+        inputBooleanEntities.Bedroom4Presence.StateChanges()
+            .WhenStateIsFor(e => e.IsOff(), TimeSpan.FromSeconds(1), scheduler)
+            .Subscribe(_ =>
+            {
+                remoteEntities.Bedroom4Remote.SendCommand("Light Power", "Bedroom 4 Fanco");
+                remoteEntities.Bedroom4Remote.SendCommand("Power", "Bedroom 4 Fanco");
+            });
     }
 }
