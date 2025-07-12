adb028a66f9a58bd30440563dea7ad954ae911ef
Qin Guan <qinguan20040914@gmail.com>
2025-07-12 23:04:55 +0800

    Remove kiosk...

--- Homer.Kiosk/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Homer.Kiosk;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
--- Homer.NetDaemon/Hubs/PrinterHub.cs
+++ Homer.NetDaemon/Hubs/PrinterHub.cs
@@ -4,4 +4,21 @@ namespace Homer.NetDaemon.Hubs;
 
 public class PrinterHub : Hub
 {
+    private readonly Dictionary<string, string> _printers = new ();
+    
+    public override async Task OnConnectedAsync()
+    {
+        
+    }
+
+    public async Task Init(string macAddr)
+    {
+        if (Context.UserIdentifier is null) return;
+        _printers[macAddr] = Context.UserIdentifier;
+    }
+
+    public async Task AddJob(string macAddr, byte[] data)
+    {
+        await Clients.User(_printers[macAddr]).SendAsync("ReceiveJob", data);
+    }
 }
