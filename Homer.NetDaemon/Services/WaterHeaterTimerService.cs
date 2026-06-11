5b33089a067e769827be00781a1722662c2c2a87
Qin Guan <qinguan20040914@gmail.com>
2026-06-11 14:18:46 +0800

    Update

--- Homer.NetDaemon/Services/WaterHeaterTimerService.cs
+++ Homer.NetDaemon/Services/WaterHeaterTimerService.cs
@@ -2,18 +2,56 @@ namespace Homer.NetDaemon.Services;
 
 public class WaterHeaterTimerService
 {
-    public DateTime? ScheduledTurnOffDateTime { get; set; }
-    public DateTime? LastTurnedOnDateTime { get; set; }
+    private DateTime? _scheduledTurnOffDateTime;
+    private DateTime? _lastTurnedOnDateTime;
+
+    public DateTime? ScheduledTurnOffDateTime
+    {
+        get => _scheduledTurnOffDateTime;
+        set
+        {
+            if (_scheduledTurnOffDateTime == value)
+            {
+                return;
+            }
+
+            _scheduledTurnOffDateTime = value;
+            StateChanged?.Invoke();
+        }
+    }
+
+    public DateTime? LastTurnedOnDateTime
+    {
+        get => _lastTurnedOnDateTime;
+        set
+        {
+            if (_lastTurnedOnDateTime == value)
+            {
+                return;
+            }
+
+            _lastTurnedOnDateTime = value;
+            StateChanged?.Invoke();
+        }
+    }
 
     public event Action<TimeSpan>? ManualOverrideRequested;
+    public event Action? StateChanged;
 
-    public void RequestManualOverride(int minutes)
+    public bool RequestManualOverride(int minutes)
     {
         if (minutes <= 0)
         {
-            return;
+            return false;
         }
 
-        ManualOverrideRequested?.Invoke(TimeSpan.FromMinutes(minutes));
+        var manualOverrideRequested = ManualOverrideRequested;
+        if (manualOverrideRequested is null)
+        {
+            return false;
+        }
+
+        manualOverrideRequested.Invoke(TimeSpan.FromMinutes(minutes));
+        return true;
     }
 }
